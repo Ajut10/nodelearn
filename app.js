9058ed82005484f80680c6398c3374f6eb66f4ea
Ajut Dangol <ajud10gol@gmail.com>
2024-09-23 07:46:44 +0545

    ejs

--- app.js
+++ app.js
@@ -9,18 +9,30 @@ app.set('view engine', 'ejs');
 //listen for requests
 app.listen(3000);
 app.get('/', (req, res)=>{
-    // res.send('<p>Welcome</p>');
-    res.sendFile('./views/index.html',{root:__dirname});
+    const blogs =[{
+
+        title : "He said" ,snippet:"Lorem ipsum dolor sit amet consectetur adipisicing elit" 
+    },{
+        title : "She said" ,snippet:"Lorem ipsum dolor sit amet consectetur adipisicing elit"    
+    },{
+        title : "We said" ,snippet:"Lorem ipsum dolor sit amet consectetur adipisicing elit" 
+    }
+]
+   
+    res.render('index',{ title : 'Home',blogs});
 });
 app.get('/about', (req, res)=>{
-    res.sendFile('./views/about.html',{root:__dirname});
+    res.render('about', { title : 'About'});
     // res.send('<p>about page</p>');
 });
 
 app.get('/about-us',(req, res)=>{
-    res.redirect('/about');
+    res.render('/about',{ title : 'About'});
+});
+app.get('/blogs/create', (req, res)=>{
+    res.render('create', { title : 'Create a new blog'});
 });
 
 app.use((req,res)=>{
-    res.sendfile('./views/404.html',{root: __dirname});
+    res.render('404',{title: 'Page not found'});
 })
--- people.js
+++
@@ -1,7 +0,0 @@
-const people = ["John", "Cena","Martha", "Adam"];
-const age =[23,32,33,43];
-
-module.exports ={people,age};
-
-const os = require("os");
-console.log(os.platform(), os.homedir());
